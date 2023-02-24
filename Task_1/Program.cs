007b826c1838091070f7ee50cbc557b46f003cc7
Alexandr Nesterenko <nesteronok@gmail.com>
2023-02-24 23:32:45 +0600

    Task 1.09

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿int[] array = GetArray(12, -9, 9);
+Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+
+}
+int sumPositive = 0; // Сумма положительных чисел
+int sumNegative = 0; // Сумма отрицательных чисел
+int size = array.Length;
+for (int i = 0; i < size; i++)
+{
+    if (array[i] > 0)
+    {
+        //sumPositive = sumPositive + array[i];
+        sumPositive += array[i];
+    }
+    else
+    {
+        sumNegative += array[i]; // sumNegative = sumNegative + array[i]
+    }
+}
+Console.WriteLine($"Сумма положительных чисел: {sumPositive}, отрицательных чисел: {sumNegative} ");
